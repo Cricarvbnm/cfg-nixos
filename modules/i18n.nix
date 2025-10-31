5281a4c0b4ae20646a01dcea94bc3980e62ecbff
Alec <alecvbnm@outlook.com>
2025-10-31 19:02:53 +0800

    feat(im): Add fctix5

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -1,6 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   i18n = {
     extraLocales = [ "zh_CN.UTF-8/UTF-8" ];
     defaultLocale = "zh_CN.UTF-8";
+
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        waylandFrontend = true;
+        addons = with pkgs; [ fcitx5-gtk fcitx5-mozc fcitx5-chinese-addons ];
+      };
+    };
   };
 }
