3309f95bb1eb32c63600375bb182c1ec933ca200
Alec <alecvbnm@outlook.com>
2025-10-31 16:00:29 +0800

    Remove drivers and packages

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,15 +4,15 @@
   imports = [ inputs.nixos-hardware.nixosModules.asus-fx506hm ];
 
   # Graphic
-  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vpl-gpu-rt
-      intel-compute-runtime
-    ];
-  };
+  # services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+  # hardware.graphics = {
+  #   enable = true;
+  #   extraPackages = with pkgs; [
+  #     intel-media-driver
+  #     vpl-gpu-rt
+  #     intel-compute-runtime
+  #   ];
+  # };
   environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 
   hardware.enableRedistributableFirmware = true;
