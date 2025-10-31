290e11c0262c0f31c4a9b480e62a7fcb76c959ed
Alec <alecvbnm@outlook.com>
2025-10-31 18:34:53 +0800

    feat(fonts): Add default fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,5 +5,14 @@
       pkgs.noto-fonts-cjk-sans
       pkgs.noto-fonts-cjk-serif
     ];
+
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "Noto Sans CJK SC" "Noto Sans CJK TC" ];
+        serif = [ "Noto Serif CJK SC" "Noto Serif CJK TC" ];
+        monospace = [ "JetBrainsMono Nerd Font Mono" ];
+      };
+    };
   };
 }
