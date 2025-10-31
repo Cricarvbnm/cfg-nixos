a4b40e41862f6166932c0bfa98e7e9b5ba6e4f3e
Alec <alecvbnm@outlook.com>
2025-10-31 21:09:31 +0800

    Add hyprlock and the user icon

--- modules/home-manager.nix/modules/de.nix/modules/compositor.nix/default.nix
+++ modules/home-manager.nix/modules/de.nix/modules/compositor.nix/default.nix
@@ -33,4 +33,13 @@
         };
     };
   };
+  # Lock
+  programs.hyprlock = {
+    enable = true;
+    extraConfig = ''
+      source = ${config.xdg.configHome}/hypr/hyprlock/current.conf
+    '';
+  };
+  xdg.configFile."hypr/hyprlock/current.conf".source =
+    config.lib.file.mkOutOfStoreSymlink ./hypr/hyprlock.conf;
 }
--- modules/home-manager.nix/users/alec.nix
+++ modules/home-manager.nix/users/alec.nix
@@ -1,1 +1,5 @@
-{ ... }: { imports = [ ../modules/de.nix ]; }
+{ ... }: {
+  imports = [ ../modules/de.nix ];
+  home.file.".icon".source =
+    /storage/General/Pictures/Heads/00028-2425649580.png;
+}
