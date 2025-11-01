ba11178e00ff2d2a5fca25cc1b51363701d88e12
Alec <alecvbnm@outlook.com>
2025-11-01 11:01:48 +0800

    refactor(fcitx): Use options to define settings

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -17,6 +17,36 @@
           fcitx5-gtk
           catppuccin-fcitx5
         ];
+
+        settings = {
+          globalOptions = {
+            "Hotkey/TriggerKeys"."0" = "Super+space";
+            "Hotkey/EnumerateGroupForwardKeys"."0" = "Alt+Super+space";
+            "Hotkey/EnumerateGroupBackwardKeys"."0" = "Alt+Super+space";
+          };
+          inputMethod = {
+            "Groups/0" = {
+              Name = "jp";
+              "Default Layout" = "us";
+              "DefaultIM" = "mozc";
+            };
+            "Groups/0/Items/0".Name = "keyboard-us";
+            "Gropus/0/Items/1".Name = "mozc";
+
+            "Groups/1" = {
+              Name = "Default";
+              "Default Layout" = "us";
+              "DefaultIM" = "pinyin";
+            };
+            "Groups/1/Items/0".Name = "keyboard-us";
+            "Gropus/1/Items/1".Name = "pinyin";
+
+            GroupOrder = {
+              "0" = "Default";
+              "1" = "jp";
+            };
+          };
+        };
       };
     };
   };
