447424fc0d68cc30a5b48e10a5865a13cd833413
Alec <alecvbnm@outlook.com>
2025-11-02 20:40:46 +0800

    feat(proxy): Open proxy port

--- modules/network.nix
+++ modules/network.nix
@@ -1,15 +1,26 @@
-{ ... }: {
+{ ... }:
+{
   networking = {
     hostName = "alec-nixos";
     networkmanager = { enable = true; };
-    proxy = {
-      default = "http://localhost:7890";
-      noProxy = "127.0.0.1,localhost,internal.domain";
-    };
   };
 
+}
+# Proxy
+// (let proxyPort = 7890;
+in {
   programs.clash-verge = {
     enable = true;
     autoStart = true;
   };
-}
+  networking = {
+    proxy = {
+      default = "http://localhost:${toString proxyPort}";
+      noProxy = "127.0.0.1,localhost,internal.domain";
+    };
+    firewall = {
+      allowedTCPPorts = [ proxyPort ];
+      allowedUDPPorts = [ proxyPort ];
+    };
+  };
+})
