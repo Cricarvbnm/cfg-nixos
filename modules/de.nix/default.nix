eae0e4965154c82e9bc60ac6b97a20167a097a24
Alec <alecvbnm@outlook.com>
2025-11-02 21:27:27 +0800

    feat(DE): Add tshark

--- modules/de.nix/default.nix
+++ modules/de.nix/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }: {
   imports =
     [ ./modules/hyprland.nix ./modules/display-manager.nix ./modules/kde.nix ];
-  environment.systemPackages = with pkgs; [ wl-clipboard ];
+  environment.systemPackages = with pkgs; [ wl-clipboard wireshark ];
+
+  programs.wireshark.enable = true;
+  users.groups.wireshark.members = [ "alec" ];
 }
