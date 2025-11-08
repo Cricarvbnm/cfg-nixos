b7e9d28b12a0efe8e5368b1d4e6253935fc54270
Alec <alecvbnm@outlook.com>
2025-11-09 00:23:23 +0800

    refactor(swaync): Flatten

--- modules/hm.nix/modules/de.nix/modules/notification.nix/default.nix
+++ modules/hm.nix/modules/de.nix/modules/notification.nix/default.nix
@@ -1,7 +1,7 @@
 { ... }: {
   services.swaync.enable = true;
   xdg.configFile = {
-    "swaync/style.css".source = ./swaync/.config/swaync/style.css;
-    "swaync/styles".source = ./swaync/.config/swaync/styles;
+    "swaync/style.css".source = ./swaync/style.css;
+    "swaync/styles".source = ./swaync/styles;
   };
 }
