1ba7f0cff37af4caad26f851e6e829a6b12369ad
Alec <alecvbnm@outlook.com>
2025-11-08 11:49:51 +0800

    feat(DM): Add sddm-astronaut-theme

--- modules/de.nix/modules/display-manager.nix
+++ modules/de.nix/modules/display-manager.nix
@@ -1,9 +1,19 @@
-{ ... }: {
-  services.displayManager = {
-    enable = true;
-    sddm = {
-      enable = true;
-      wayland.enable = true;
-    };
+{ pkgs, ... }:
+let
+  sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "hyprland_kath";
+    themeConfig = { HideSystemButtons = false; };
   };
+in {
+  services.displayManager.sddm = {
+    enable = true;
+    package = pkgs.kdePackages.sddm; # qt6 sddm version
+
+    theme = "sddm-astronaut-theme";
+    extraPackages = [ sddm-astronaut ];
+
+    wayland.enable = true;
+  };
+
+  environment.systemPackages = [ sddm-astronaut ];
 }
