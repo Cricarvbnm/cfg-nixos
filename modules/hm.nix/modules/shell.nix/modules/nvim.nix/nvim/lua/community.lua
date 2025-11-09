df1e268808c1604bc0b1457911744ea08cac9171
Alec <alecvbnm@outlook.com>
2025-11-09 15:33:15 +0800

    refactor(nvim): To be a submodule

--- modules/hm.nix/modules/shell.nix/modules/nvim.nix/nvim/lua/community.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  require("plugins.astrolsp").opts.features.inlay_hints and {
-    import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints",
-  } or {},
-}
--- modules/hm.nix/modules/shell.nix/modules/nvim.nix/nvim/lua/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  ---@type LazySpec
-  spec = {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 10000,
-  },
-  name = "catppuccin-macchiato",
-}
