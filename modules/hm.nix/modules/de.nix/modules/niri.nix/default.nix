feb10126e8cc2de8618153c1e76bcb8e9f30b11b
Alec <alecvbnm@outlook.com>
2025-11-09 23:51:59 +0800

    feat: sync submodules

--- modules/hm.nix/modules/de.nix/modules/niri.nix/default.nix
+++ modules/hm.nix/modules/de.nix/modules/niri.nix/default.nix
@@ -1,3 +1,14 @@
 { config, ... }: {
+  imports = [
+    ../bar.nix
+    ../lock.nix
+    ../idle.nix
+    ../notification.nix
+    ../clipboard.nix
+    ../menu.nix
+    ../polkit.nix
+    .../wallpaper.nix
+  ];
+
   xdg.configFile.niri.source = config.lib.file.mkOutOfStoreSymlink ./niri;
 }
