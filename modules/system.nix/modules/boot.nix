12b63571d40c5efd790fc3c2e37db015fb7b78f1
Alec <alecvbnm@outlook.com>
2025-11-12 23:14:53 +0800

    feat(kernel): Use zen kernel

--- modules/system.nix/modules/boot.nix
+++ modules/system.nix/modules/boot.nix
@@ -1,4 +1,10 @@
-{ ... }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+{ pkgs, ... }: {
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelPackages = pkgs.linuxPackages_zen;
+  };
 }
