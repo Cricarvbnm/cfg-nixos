01776addf19403d31c043e69690d8dfb66ad41c9
Alec <alecvbnm@outlook.com>
2025-11-12 19:16:08 +0800

    feat(docker): Add docker-compose

--- modules/system.nix/modules/docker.nix
+++ modules/system.nix/modules/docker.nix
@@ -1,4 +1,6 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   virtualisation.docker = { enable = true; };
   users.groups.docker.members = [ config.mine.users.users.mainUser ];
+
+  environment.systemPackages = with pkgs; [ docker-compose ];
 }
