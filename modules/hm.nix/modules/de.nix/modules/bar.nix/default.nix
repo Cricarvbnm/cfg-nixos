b9e9cde6644e2b1c7ae3234f374d97cdb25b7801
Alec <alecvbnm@outlook.com>
2025-11-13 14:26:41 +0800

    feat(waybar): Add nm-applet and xdg-launch as dependencies

--- modules/hm.nix/modules/de.nix/modules/bar.nix/default.nix
+++ modules/hm.nix/modules/de.nix/modules/bar.nix/default.nix
@@ -1,15 +1,18 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   programs.waybar.enable = true;
   xdg.configFile."waybar".source = config.lib.file.mkOutOfStoreSymlink ./waybar;
 
+  # Need to be launched by compositors
   xdg.desktopEntries = {
     "waybar" = {
-      exec = "waybar";
       name = "Waybar";
+      exec = "waybar";
     };
     "waybar-bot" = {
       name = "Waybar Bottom";
       exec = "waybar --config ${config.xdg.configHome}/waybar/bot.jsonc";
     };
   };
+
+  home.packages = with pkgs; [ networkmanagerapplet xdg-launch ];
 }
