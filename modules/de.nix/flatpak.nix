b8e2f348d8afd2b37f71e9dd65831b47cd7ec783
Alec <alecvbnm@outlook.com>
2025-11-15 20:42:32 +0800

    feat(flatpak)Daily update

--- modules/de.nix/flatpak.nix
+++ modules/de.nix/flatpak.nix
@@ -1,4 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   services.flatpak.enable = true;
   environment.systemPackages = with pkgs; [ kdePackages.discover ];
+
+  systemd.services.flatpak-update = {
+    unitConfig = rec {
+      After = "network-online.target";
+      Requisite = After;
+    };
+    serviceConfig = {
+      ExecStart =
+        "${config.services.flatpak.package}/bin/flatpak update --assumeyes";
+      Type = "oneshot";
+    };
+
+    startAt = "00:00:00";
+  };
 }
