51c305a0e165a2ca3484cabf03b801793a1e09d3
Alec <alecvbnm@outlook.com>
2025-11-16 15:22:57 +0800

    feat(nix)Automatic gc and optimization

--- modules/system.nix/nix.nix
+++ modules/system.nix/nix.nix
@@ -5,5 +5,15 @@
 
       max-jobs = auto
     '';
+
+    optimise = {
+      automatic = true;
+      dates = [ "00:30" ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
   };
 }
