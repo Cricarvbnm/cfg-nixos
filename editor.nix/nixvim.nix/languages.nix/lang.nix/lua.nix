bfdd8f43e2311b1889cca1a66e59714d5f8e828b
alechron <alechron@outlook.com>
2026-03-24 15:49:37 +0800

    feat(editor/languages): add stylua formatter for lua

--- editor.nix/nixvim.nix/languages.nix/lang.nix/lua.nix
+++ editor.nix/nixvim.nix/languages.nix/lang.nix/lua.nix
@@ -6,4 +6,6 @@
     lintersByFt.lua = [ "luacheck" ];
     linters.luacheck = { cmd = lib.getExe pkgs.luaPackages.luacheck; };
   };
+
+  plugins.conform-nvim.settings.formatters_by_ft.lua = [ "stylua" ];
 }
