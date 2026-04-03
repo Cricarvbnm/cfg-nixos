e671ca534a0b93286f3cfe80c921c1fc3dc7d62f
alechron <alechron@outlook.com>
2026-04-03 19:43:44 +0800

    fix(editor/language/util/opt-tab): Try coercing an integer and a boolean

--- editor.nix/nixvim.nix/languages.nix/lang.nix/utils.nix/opt-tab.nix
+++ editor.nix/nixvim.nix/languages.nix/lang.nix/utils.nix/opt-tab.nix
@@ -6,10 +6,13 @@
   tab-width ? 4,
   expandtab ? true,
 }:
+let
+  tab-width-str = builtins.toString tab-width;
+in
 {
   text = ''
-    vim.opt_local.tabstop = ${tab-width}
-    vim.opt_local.shiftwidth = ${tab-width}
-    vim.opt_local.expandtab = ${expandtab}
+    vim.opt_local.tabstop = ${tab-width-str}
+    vim.opt_local.shiftwidth = ${tab-width-str}
+    vim.opt_local.expandtab = ${builtins.toString expandtab}
   '';
 }
