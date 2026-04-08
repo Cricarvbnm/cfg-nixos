c7d1429a14f91dc89c21a3246430b921e6f0c96b
alechron <alechron@outlook.com>
2026-04-08 14:20:35 +0800

    feat(proxy/ssh): Proxy ssh protocol

--- network.nix/proxy.nix
+++ network.nix/proxy.nix
@@ -1,12 +1,25 @@
+{ pkgs, lib, ... }:
+let
+  port = 7890;
+  inherit (builtins) toString;
+in
 {
-  programs.clash-verge = {
-    enable = true;
-    autoStart = true;
-    serviceMode = true;
+  programs = {
+    clash-verge = {
+      enable = true;
+      autoStart = true;
+      serviceMode = true;
+    };
+
+    # Proxy for SSH clients
+    ssh.extraConfig = ''
+      Host !*.local *
+        ProxyCommand ${lib.getExe pkgs.netcat} -Xconnect -x localhost:${toString port} %h %p
+    '';
   };
 
   networking.proxy = {
-    default = "http://localhost:7890";
+    default = "http://localhost:${toString port}";
     noProxy = "127.0.0.1,localhost,internal.domain";
   };
 }
