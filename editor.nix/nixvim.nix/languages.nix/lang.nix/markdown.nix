0932e1ff5368c612a180cb099466a7d9ca01dcb1
alechron <alechron@outlook.com>
2026-04-21 14:39:21 +0800

    remove(editor/markdown): lintter

--- editor.nix/nixvim.nix/languages.nix/lang.nix/markdown.nix
+++ editor.nix/nixvim.nix/languages.nix/lang.nix/markdown.nix
@@ -1,13 +1,5 @@
-{ lib, pkgs, ... }:
 {
   lsp.servers.marksman.enable = true;
-
-  plugins.lint = {
-    lintersByFt.markdown = [ "vale" ];
-    linters.vale = {
-      cmd = lib.getExe pkgs.vale;
-    };
-  };
 
   plugins = {
     conform-nvim.settings.formatters_by_ft.markdown = [ "prettier" ];
